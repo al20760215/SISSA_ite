d70baca4fd03177ef560641374d7e40554d32084
WilliamR <al20760219@ite.edu.mx>
2024-10-26 01:21:49 -0700

    actualizada pantalla de login con imagen de fondo, formularios y redireccion en base al rol

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,8 +8,10 @@ import {
   Card,
   CardActions,
   CardContent,
+  TextField,
 } from "@mui/material";
 import Grid from "@mui/material/Grid2";
+import loginbg from "../assets/ensenada.jpg";
 
 export default function HomePage() {
   const navigate = useNavigate();
@@ -34,15 +36,61 @@ export default function HomePage() {
   });
 
   return (
-    <Grid container spacing={{ xs: 2, md: 3 }}>
-      <Grid size={{ xs: 12 }}>1</Grid>
-      <Grid size={{ xs: 12, md: 4 }}>
-        <Container>
-          <Typography variant="h5">Login</Typography>
-          <p>Esta es la pagina inicial</p>
-          <Button variant="contained">Login</Button>
-        </Container>
-      </Grid>
-    </Grid>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh", //vh = viewport height
+        backgroundImage: `url(${loginbg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        padding: { xs: 2, sm: 0 },
+      }}
+    >
+      <Card
+        sx={{
+          width: { xs: "100%", sm: "70%", md: "50%", lg: "30%" }, // breakpoints
+          my: { xs: 2, sm: 0 }, // Vertical margin on small screens
+          p: { xs: 2, sm: 4 }, // Smaller padding on small screens
+          boxShadow: 3,
+          maxWidth: "90vw", // vh = viewport width
+          minWidth: 300,
+        }}
+      >
+        <CardContent>
+          <Typography variant="h5" align="center" color="textPrimary">
+            SISTEMA INTEGRAL DE SERVICIO SOCIAL ALBATROS
+          </Typography>
+          <Typography color="textSecondary" gutterBottom>
+            INSTITUTO TECNOLOGICO DE ENSENADA 2024
+          </Typography>
+          <TextField
+            label="USUARIO"
+            fullWidth
+            variant="outlined"
+            margin="normal"
+          ></TextField>
+          <TextField
+            label="CONTRASEÑA"
+            fullWidth
+            variant="outlined"
+            type="password"
+            margin="normal"
+          ></TextField>
+          <Button variant="contained" sx={{ mt: 2, mx: 4 }}>
+            Iniciar sesion
+          </Button>
+          <Button variant="outlined" sx={{ mt: 2 }}>
+            registrarse
+          </Button>
+        </CardContent>
+      </Card>
+    </Box>
   );
 }
+
+/* <Grid container spacing={{ xs: 2, md: 3 }}>
+  <Grid size={{ xs: 12 }}>1</Grid>
+  <Grid size={{ xs: 12, md: 4 }}></Grid>
+</Grid>; */
